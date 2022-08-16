28a8db83507cc0210c18793663e4ba4a41f96432
Gabriel <gabmarod@gmail.com>
2022-08-16 18:25:14 +0200

    Get random computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+
+let choicesArray = ["Rock", "Paper", "Scissors"]
+
+var randomNumber = Math.floor(Math.random()*choicesArray.length);
+
+
+function getComputerChoice() {
+    return randomNumber;
+}
+
+const computerSelection = getComputerChoice();
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
