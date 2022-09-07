84a0f7916f1bd643746391cbd5b8a5afef7e2638
Gabriel <gabmarod@gmail.com>
2022-09-07 14:01:30 +0200

    Write base for the first round

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const choicesArray = ["Rock", "Paper", "Scissors"]
+const choicesArray = ["rock", "paper", "scissors"]
 
 // program to get a random item from an array
 
@@ -14,10 +14,32 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+let userInput = window.prompt('Choose one! Rock, Paper or Scissors?');
+
+function playerSelection(){
+    let playerSelection = userInput.toLowerCase();
+    return playerSelection;
+}
 
 const computerSelection = getComputerChoice();
 
 
 function playRound(playerSelection, computerSelection) {
-
+    if (playerSelection === "rock" & computerSelection === "paper"){
+        alert("You Lose! Paper beats Rock")
+    } else if (playerSelection === "rock" & computerSelection === "scissors"){
+        alert("You Win! Rock beats Scissors")
+    } else if (playerSelection === "paper" & computerSelection === "rock"){
+        alert("You Win! Paper beats Rock")
+    } else if (playerSelection === "paper" & computerSelection === "scissors"){
+        alert("You Lose! Scissors beats Paper")
+    } else if (playerSelection === "scissors" & computerSelection === "rock"){
+        alert("You Lose! Rock beats Scissors")
+    } else if (playerSelection === "scissors" & computerSelection === "paper"){
+        alert("You Win! Scissors beats Paper")
+    } else {
+        alert("Try again! That's a tie")
+    }
 }
+
+playRound();
