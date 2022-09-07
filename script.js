74caa5469f66ccff3f7852aecdc7338842bf73db
Gabriel <gabmarod@gmail.com>
2022-09-07 13:05:50 +0200

    Fix issue in function getComputerChoice

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
-let choicesArray = ["Rock", "Paper", "Scissors"]
+const choicesArray = ["Rock", "Paper", "Scissors"]
 
-var randomNumber = Math.floor(Math.random()*choicesArray.length);
-
+// program to get a random item from an array
 
 function getComputerChoice() {
-    return randomNumber;
+
+    // get random index value
+    const randomIndex = Math.floor(Math.random() * choicesArray.length);
+
+    // get random item
+    const computerChoice = choicesArray[randomIndex];
+
+    return computerChoice;
 }
+
 
 const computerSelection = getComputerChoice();
 
