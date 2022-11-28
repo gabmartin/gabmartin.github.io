1ddccfa08a9863163f9b51f3ca5c8084117d9b73
gabmartin <97696521+gabmartin@users.noreply.github.com>
2022-11-28 18:47:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 
-const choicesArray = ["rock", "paper", "scissors"];
 
-// program to get a random item from an array
+const choicesArray = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+
+// Funcion que elige una opcion 
 
 function getComputerChoice() {
 
-    // get random item
     let computerChoice = choicesArray[Math.floor(Math.random() * choicesArray.length)];
 
     return computerChoice;
 }
 
+// Una ronda del juego que anuncia el resultado y la puntuacion.
+
 function playRound(userInput, computerSelection) {
     if (userInput === computerSelection) {
         return ("It's a draw!");
       } else if ((userInput === "rock") && (computerSelection === "scissors")) {
+        playerScore++;
         return ("You win! Rock beats scissors");
       } else if (userInput === "rock" && computerSelection === "paper") {
+        computerScore++;
         return ("You lose! Paper beats rock");
       } else if (userInput === "paper" && computerSelection === "rock") {
+        playerScore++;
         return ("You win! Paper beats rock");
       } else if (userInput === "paper" && computerSelection === "scissors") {
+        computerScore++;
         return ("You lose! Scissors beat paper");
       } else if (userInput === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return ("You win! Scissors beat paper");
       } else if (userInput === "scissors" && computerSelection === "rock") {
+        computerScore++;
         return ("You lose!Rock beats scissors");
       }
     }
@@ -33,5 +43,10 @@ function playRound(userInput, computerSelection) {
 let userInput = window.prompt('Choose one! Rock, Paper or Scissors?').toLowerCase();
 let computerSelection = getComputerChoice();
 
+
+// Comprobaciones de funcionamiento en la consola.
+
 console.log(computerSelection);
-console.log(playRound(userInput, computerSelection))
+console.log(playRound(userInput, computerSelection));
+console.log(playerScore);
+console.log(computerScore);
